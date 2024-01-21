958e9b3aca4952dfcf69912a3c94742cba5bd6a7
btarg <icrazyblaze1@gmail.com>
2024-01-21 16:35:29 +0000

    Refactor tools

--- src/main/java/io/github/btarg/javaOpenAI/Main.java
+++ src/main/java/io/github/btarg/javaOpenAI/Main.java
@@ -5,6 +5,8 @@ import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
+import io.github.btarg.javaOpenAI.memory.PersistentChatMemoryStore;
+import io.github.btarg.javaOpenAI.tools.Calculator;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,27 +33,6 @@ public class Main {
     interface Assistant {
 
         String chat(String userMessage);
-    }
-
-    static class Calculator {
-
-        @Tool("Calculates the length of a string")
-        int stringLength(String s) {
-            System.out.println("Called stringLength with s='" + s + "'");
-            return s.length();
-        }
-
-        @Tool("Calculates the sum of two numbers")
-        int add(int a, int b) {
-            System.out.println("Called add with a=" + a + ", b=" + b);
-            return a + b;
-        }
-
-        @Tool("Calculates the square root of a number")
-        double sqrt(int x) {
-            System.out.println("Called sqrt with x=" + x);
-            return Math.sqrt(x);
-        }
     }
 
 }
--- src/main/java/io/github/btarg/javaOpenAI/memory/PersistentChatMemoryStore.java
+++ src/main/java/io/github/btarg/javaOpenAI/memory/PersistentChatMemoryStore.java
@@ -1,4 +1,4 @@
-package io.github.btarg.javaOpenAI;
+package io.github.btarg.javaOpenAI.memory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
